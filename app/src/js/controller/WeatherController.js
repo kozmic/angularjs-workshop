6397c57a00c8812ba5f4e8d02dbade0a1ea02e57
Ståle Pettersen <staale@gmail.com>
2013-05-11 20:37:27 +0200

    Cleaner param definition to . Added some geocode code. (currently not in use)

--- app/src/js/controller/WeatherController.js
+++ app/src/js/controller/WeatherController.js
@@ -2,16 +2,31 @@
 
 angular.module('waControllers', [])
 
-    .controller('WeatherController', ['$scope', 'WeatherService',
-        function ($scope, WeatherService) {
+    .controller('WeatherController', ['$scope', 'WeatherService', function ($scope, WeatherService) {
 
-        $scope.heading = 'Awesome weather application';
+            $scope.heading = 'Awesome weather application';
 
-        $scope.search = { city: 'Oslo' };
+            $scope.search = { city: 'Oslo' };
 
-        $scope.doSearch = function () {
+            $scope.doSearch = function () {
+                if($scope.searchForm.$valid){
+                    $scope.searchResult = WeatherService.query({ q: $scope.search.city });
+//            $scope.searchResult = WeatherService.query();
 
-            $scope.searchResult = WeatherService.query({ q: $scope.search.city });
-        }
-    }])
+                }
+
+
+            }
+
+
+            if ("geolocation" in navigator) {
+                // Fetches location only once, so no support for Superman.
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    $scope.search.geo = { lat: position.coords.latitude, long: position.coords.longitude };
+                });
+                $scope.$watch('search.geo', function (newValue, oldValue) {
+                    console.log(newValue, oldValue);
+                });
+            }
+        }])
 ;
--- app/src/js/service/WeatherService.js
+++ app/src/js/service/WeatherService.js
@@ -4,9 +4,20 @@ angular.module('waServices', [])
 
     .factory('WeatherService', ['$resource', function ($resource) {
 
-        return $resource('http://api.openweathermap.org/data/2.5/weather?mode=json&units=metric&q=:q&callback=:callback',
-            { callback: 'JSON_CALLBACK' },
-            { query: { method: 'JSONP', isArray: false} }
+
+//        return $resource('http://api.openweathermap.org/data/2.5/weather?mode=json&units=metric&lat=59.9193233&lon=-10.8166766&callback=:callback',
+        return $resource('http://api.openweathermap.org/data/2.5/weather',
+            {},
+            {
+                query: {
+                    method: 'JSONP',
+                    params: {
+                        mode: 'json',
+                        units: 'metric',
+                        callback: 'JSON_CALLBACK'
+                    }
+                }
+            }
         );
 
     }])
