3a07f411fd080d85aea6cd5e336fbce60a433b5a
Ståle Pettersen <staale@gmail.com>
2013-05-14 13:59:32 +0200

    More up to date mock data

--- app/src/js/mock/searchResults_today.js
+++ app/src/js/mock/searchResults_today.js
@@ -14,3 +14,11 @@ window.searchResult_today_Oslo = {
     "name": "Oslo",
     "cod": 200
 };
+
+window.searchResult_today_Trondheim = {
+    "coord":{"lon":10.39506,"lat":63.430489},"sys":{"country":"NO","sunrise":1368497481,"sunset":1368562689},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"base":"global stations","main":{"temp":11.04,"humidity":57,"pressure":998,"temp_min":11,"temp_max":11.11},"wind":{"speed":1.54,"gust":3.6,"deg":29},"clouds":{"all":36},"dt":1368531633,"id":3133880,"name":"Trondheim","cod":200
+};
+
+window.searchResult_today_Hawaii = {
+    "coord":{"lon":-157.858,"lat":21.3048},"sys":{"country":"United States of America","sunrise":1368546786,"sunset":1368594147},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}],"base":"global stations","main":{"temp":23,"pressure":1019,"humidity":78,"temp_min":23,"temp_max":23},"wind":{"speed":1.5,"deg":60},"clouds":{"all":90},"dt":1368528900,"id":5856195,"name":"","cod":200
+};
